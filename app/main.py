e1746aab2c918d41fa4f2f407a89e079943771bd
nazifishrak <nzfishrak60@gmail.com>
2023-10-07 01:41:12 -0700

    CRUD started

--- app/main.py
+++ app/main.py
@@ -82,10 +82,10 @@ def get_latest_post():
 
 
 @app.get("/posts/{id}")
-def get_post(id, response: Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+def get_post(id:int, response: Response):
+    cursor.execute("""SELECT * FROM posts where id = %s""",(str(id)))
+    post=cursor.fetchone()
+    conn.commit()
 
     return {"post_detail": post}
 

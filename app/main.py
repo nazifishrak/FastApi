06a1c0301cb14204b545375989885ea66f1272c4
nazifishrak <nzfishrak60@gmail.com>
2023-10-07 00:58:15 -0700

    Connected to db using psycopg2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 # This imports fastAPI library
 from email.policy import HTTP
 from random import randrange
-from typing import Optional
+import time
+from typing import Optional, Tuple
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi import Body
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import psycopg
 
 app = FastAPI()
 
@@ -13,6 +17,24 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int]= None
+
+
+# with psycopg.connect(host="localhost",dbname="postgres", user="postgres", password="password") as conn:
+#     with conn.cursor(row_factory=psycopg.AsyncCursor) as curr:
+#         print("Database connection was successful")
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password = 'password', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection successful")
+        break
+    except Exception as err:
+        print("Connecting to database failed")
+        print(err)
+        time.sleep(2)
+
+
 
 my_posts = [{"title": "Title1", "content": "Content1", "id":1}, {"title": "Title2", "content": "Content2", "id":2}]
 def find_post(id: str):
@@ -36,17 +58,21 @@ async def root():
 #If we go to the http://127.0.0.1:8000/posts we will see the json output
 @app.get("/posts")
 def get_posts():
+    cursor.execute("""SELECT * from posts""")
+    posts=cursor.fetchall()
+    return {"data":posts}
+
     # Fast api will auto serialize my_posts into JSON
-    return {"data": my_posts}
+    
 
 @app.post("/post",status_code=status.HTTP_201_CREATED)  
-def create_posts(new_post: Post):
-    post_dict = new_post.model_dump()
-    post_dict['id']= randrange(0,1000000)
-    my_posts.append(post_dict)
-    print(my_posts)
-    # new_post.dict() -> returns the post as a dict
-    return {"data": post_dict}
+def create_posts(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published)
+                   VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+
+    conn.commit()
+    return {"data": new_post}
 
 @app.get("/posts/latest")
 def get_latest_post():
@@ -73,4 +99,16 @@ def delete_post(id: int):
     my_posts.pop(index)
     print(my_posts)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index= find_post_index(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id{id} does not exist")
+    post_dict = post.model_dump()
+
+    post_dict['id'] = id
+    my_posts[index]=post_dict
+    return {'data': post_dict}
+
 
